dbda06175f29f0a85394ef82b655cbb92d3255d9
Ulrik Strid <ulrik.strid@outlook.com>
2017-08-14 13:17:29 +0200

    Update to same state as PR

--- src/azureFunctionsApollo.ts
+++ src/azureFunctionsApollo.ts
@@ -1,10 +1,10 @@
 import {
   HttpContext,
   IFunctionRequest,
-  HttpStatusCodes
-} from "azure-functions-typescript";
-import { GraphQLOptions, runHttpQuery } from "apollo-server-core";
-import * as GraphiQL from "apollo-server-module-graphiql";
+  HttpStatusCodes,
+} from 'azure-functions-typescript';
+import { GraphQLOptions, runHttpQuery } from 'apollo-server-core';
+import * as GraphiQL from 'apollo-server-module-graphiql';
 
 export interface AzureFunctionsGraphQLOptionsFunction {
   (context: HttpContext): GraphQLOptions | Promise<GraphQLOptions>;
@@ -15,106 +15,112 @@ export interface AzureFunctionsHandler {
 }
 
 export interface IHeaders {
-  "content-type"?: string;
-  "content-length"?: HttpStatusCodes | number;
-  "content-disposition"?: string;
-  "content-encoding"?: string;
-  "content-language"?: string;
-  "content-range"?: string;
-  "content-location"?: string;
-  "content-md5"?: Buffer;
-  "expires"?: Date;
-  "last-modified"?: Date;
+  'content-type'?: string;
+  'content-length'?: HttpStatusCodes | number;
+  'content-disposition'?: string;
+  'content-encoding'?: string;
+  'content-language'?: string;
+  'content-range'?: string;
+  'content-location'?: string;
+  'content-md5'?: Buffer;
+  'expires'?: Date;
+  'last-modified'?: Date;
   [header: string]: any;
 }
 
 export interface AzureFunctionsGraphiQLOptionsFunction {
-  (context: HttpContext):
-    | GraphiQL.GraphiQLData
-    | Promise<GraphiQL.GraphiQLData>;
+  (context: HttpContext, request: IFunctionRequest):
+      | GraphiQL.GraphiQLData
+      | Promise<GraphiQL.GraphiQLData>;
 }
 
 export function graphqlAzureFunctions(
-  options: GraphQLOptions | AzureFunctionsGraphQLOptionsFunction
+  options: GraphQLOptions | AzureFunctionsGraphQLOptionsFunction,
 ): AzureFunctionsHandler {
   if (!options) {
-    throw new Error("Apollo Server requires options.");
+      throw new Error('Apollo Server requires options.');
   }
 
   if (arguments.length > 1) {
-    throw new Error(
-      `Apollo Server expects exactly one argument, got ${arguments.length}`
-    );
+      throw new Error(
+          `Apollo Server expects exactly one argument, got ${arguments.length}`,
+      );
   }
 
-  return async (httpContext: HttpContext, request: IFunctionRequest) => {
-    let query = request.method === "POST" ? request.body : request.query,
-      statusCode: number = null,
-      gqlResponse = null,
-      headers: { [headerName: string]: string } = {};
+  return (httpContext: HttpContext, request: IFunctionRequest) => {
+      const queryRequest = {
+          method: request.method,
+          options: options,
+          query: request.method === 'POST' ? request.body : request.query,
+      };
 
-    if (query && typeof query === "string") {
-      query = JSON.parse(query);
-    }
-
-    try {
-      gqlResponse = await runHttpQuery([httpContext, request], {
-        method: request.method,
-        options: options,
-        query: query
-      });
-      headers["Content-Type"] = "application/json";
-      statusCode = 200;
-    } catch (error) {
-      if ("HttpQueryError" !== error.name) {
-        throw error;
+      if (queryRequest.query && typeof queryRequest.query === 'string') {
+          queryRequest.query = JSON.parse(queryRequest.query);
       }
 
-      headers = error.headers;
-      statusCode = error.statusCode;
-      gqlResponse = error.message;
-    } finally {
-      httpContext.done(null, {
-        status: statusCode,
-        headers: headers,
-        body: gqlResponse
-      });
-    }
+      return runHttpQuery([httpContext, request], queryRequest)
+          .then(gqlResponse => {
+              const result = {
+                  status: 200,
+                  headers: { 'Content-Type': 'application/json' },
+                  body: gqlResponse,
+              };
+
+              httpContext.res = result;
+
+              httpContext.done(null, result);
+          })
+          .catch(error => {
+              const result = {
+                  status: error.statusCode,
+                  headers: error.headers,
+                  body: error.message,
+              };
+
+              httpContext.res = result;
+
+              httpContext.done(error, result);
+          });
   };
 }
 
 /* This Azure Functions Handler returns the html for the GraphiQL interactive query UI
- *
- * GraphiQLData arguments
- *
- * - endpointURL: the relative or absolute URL for the endpoint which GraphiQL will make queries to
- * - (optional) query: the GraphQL query to pre-fill in the GraphiQL UI
- * - (optional) variables: a JS object of variables to pre-fill in the GraphiQL UI
- * - (optional) operationName: the operationName to pre-fill in the GraphiQL UI
- * - (optional) result: the result of the query to pre-fill in the GraphiQL UI
- */
+*
+* GraphiQLData arguments
+*
+* - endpointURL: the relative or absolute URL for the endpoint which GraphiQL will make queries to
+* - (optional) query: the GraphQL query to pre-fill in the GraphiQL UI
+* - (optional) variables: a JS object of variables to pre-fill in the GraphiQL UI
+* - (optional) operationName: the operationName to pre-fill in the GraphiQL UI
+* - (optional) result: the result of the query to pre-fill in the GraphiQL UI
+*/
 
 export function graphiqlAzureFunctions(
-  options: GraphiQL.GraphiQLData | AzureFunctionsGraphiQLOptionsFunction
+  options: GraphiQL.GraphiQLData | AzureFunctionsGraphiQLOptionsFunction,
 ) {
   return (httpContext: HttpContext, request: IFunctionRequest) => {
-    const query = request.query;
-    GraphiQL.resolveGraphiQLString(query, options, httpContext, request).then(
-      graphiqlString => {
-        httpContext.done(null, {
-          statusCode: 200,
-          headers: {
-            "Content-Type": "text/html"
+      const query = request.query;
+
+      GraphiQL.resolveGraphiQLString(query, options, httpContext, request).then(
+          graphiqlString => {
+              httpContext.res = {
+                  status: 200,
+                  headers: {
+                      'Content-Type': 'text/html',
+                  },
+                  body: graphiqlString,
+              };
+
+              httpContext.done(null, httpContext.res);
           },
-          body: graphiqlString
-        });
-      },
-      error => {
-        httpContext.done(null, {
-          statusCode: 500,
-          body: error.message
-        });
-      }
-    );
+          error => {
+              httpContext.res = {
+                  status: 500,
+                  body: error.message,
+              };
+
+              httpContext.done(error, httpContext.res);
+          },
+      );
   };
 }
